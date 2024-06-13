6108022d3c927f9b9a67fd0f73dce35c5bfc0673
Avinash Pandey <avinash.12009084@lpu.in>
2024-06-13 23:57:27 +0530

    Small Fixes

--- client/src/components/common/LoadingSpinner.jsx
+++ client/src/components/common/LoadingSpinner.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+// import Typewriter from "typewriter-effect";
 
 const LoadingSpinner = ({ size = "md" }) => {
   const sizeClass = `loading-${size}`;
   return (
     <>
+      {/* <div className="text-orange-500 text-2xl text-bold">
+        <Typewriter
+          options={{
+            strings: ["Sorry, 😥 We are not available at this time!"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </div> */}
       <span className={`loading loading-dots ${sizeClass}`}></span>
     </>
   );
